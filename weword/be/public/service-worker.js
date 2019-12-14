5fe096c5bfaaf825f837f9bb81ce82415836ce76
nfiner <nfiner@umich.edu>
2019-12-14 15:46:06 -0500

    Actual mobile fix

--- weword/be/public/service-worker.js
+++ weword/be/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e979ff91301f575a370122b69c5b8f4.js"
+  "/precache-manifest.0aee7c44860b660f3bca50236142b821.js"
 );
 
 self.addEventListener('message', (event) => {
--- weword/fe/build/precache-manifest.0aee7c44860b660f3bca50236142b821.js
+++ weword/fe/build/precache-manifest.0aee7c44860b660f3bca50236142b821.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "893824df189075cf9f09803409ccedc4",
+    "revision": "a28a1ba3c7db716646e5e89b257f1ad6",
     "url": "/index.html"
   },
   {
-    "revision": "1be7519070cec6b999d7",
-    "url": "/static/css/main.4b9ceb9b.chunk.css"
+    "revision": "69fb61a6f4d084a10503",
+    "url": "/static/css/main.f4730639.chunk.css"
   },
   {
     "revision": "1fcf5fbc2f172e762a31",
     "url": "/static/js/2.344f2893.chunk.js"
   },
   {
-    "revision": "1be7519070cec6b999d7",
+    "revision": "69fb61a6f4d084a10503",
     "url": "/static/js/main.f48d89ad.chunk.js"
   },
   {
